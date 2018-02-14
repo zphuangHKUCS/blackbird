0a2ecba4308cdb818aa73b0b69a145910f60133b
Cody Kessler <codykessler@gmail.com>
2018-02-14 11:15:24 -0600

    merge in master (#2)
    * Add binance.cpp/h
    
    * conform to old getActivePos
    
    * Add to parameters.cpp/h
    
    * Add exchange to main.cpp
    
    * Add to conf
    
    * Readme
    
    * add return to Binance::getActivePos
    
    * Fix Kraken getLimitPrice
    
    * forgot .get() in kraken.cpp
    
    * forgot array declarations in main.cpp
    
    * test conf in gitignore
    
    * test conf in gitignore again
    
    * general array fixes and a test.conf in gitignore
    
    * Poloniex integration
    
    * poloniex fixes
    
    * Add direct debug launch config for VScode

--- src/exchanges/kraken.cpp
+++ src/exchanges/kraken.cpp
@@ -17,8 +17,7 @@ namespace Kraken
 // Initialise internal variables
 static unique_json krakenTicker = nullptr;
 static bool krakenGotTicker = false;
-static unique_json krakenLimPrice = nullptr;
-static bool krakenGotLimPrice = false;
+
 
 static RestApi &queryHandle(Parameters &params)
 {
@@ -184,14 +183,9 @@ double getActivePos(Parameters &params, std::string orderId)
 
 double getLimitPrice(Parameters &params, double volume, bool isBid)
 {
-  if (!krakenGotLimPrice)
-  {
-    auto &exchange = queryHandle(params);
-    krakenLimPrice.reset(exchange.getRequest("/0/public/Depth?pair=XXBTZUSD"));
-  }
-  krakenGotLimPrice = !krakenGotLimPrice;
-  auto root = krakenLimPrice.get();
-  auto branch = json_object_get(json_object_get(root, "result"), "XXBTZUSD");
+  auto &exchange = queryHandle(params);
+  unique_json root { exchange.getRequest("/0/public/Depth?pair=XXBTZUSD") };
+  auto branch = json_object_get(json_object_get(root.get(), "result"), "XXBTZUSD");
   branch = json_object_get(branch, isBid ? "bids" : "asks");
 
   // loop on volume
